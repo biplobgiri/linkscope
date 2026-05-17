ea0b3cb47ff6f12edfe3ede9bccbc1c6077efbbb
Biplobgiri <biplobgiri92@gmail.com>
2026-05-17 15:04:12 +0545

    link validation changed

--- app/schemas.py
+++ app/schemas.py
@@ -3,10 +3,17 @@ from datetime import datetime
 from typing import Optional
 
 class ShortenRequest(BaseModel):
-    original_url: HttpUrl
+    original_url: str
     custom_slug: Optional[str] = None
     max_clicks: Optional[int] = None
     expires_at: Optional[datetime] = None
+
+    @field_validator("original_url")
+    @classmethod
+    def validate_url(cls,v):
+        if not v.startswith("http://") and not v.startswith("https://"):
+            v="https://"+v
+        return v
 
 
 
@@ -17,12 +24,7 @@ class ShortenResponse(BaseModel):
     expires_at: Optional[datetime] = None
     max_clicks: Optional[int] = None
 
-    @field_validator("original_url")
-    @classmethod
-    def validate_url(cls,v):
-        if not v.startswith("http://") and not v.startswith("https://"):
-            v="https://"+v
-        return v
+
      
 class ClickResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
