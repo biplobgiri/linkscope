c6e23744a7b777b0194db390693dfa2e9e389b3d
Biplobgiri <biplobgiri92@gmail.com>
2026-05-17 13:40:27 +0545

    db fix

--- app/database.py
+++ app/database.py
@@ -5,20 +5,21 @@ import os
 
 load_dotenv()
 
-DATABASE_URL= os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine= create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 
-SessionLocal= sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 class Base(DeclarativeBase):
     pass
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-        
